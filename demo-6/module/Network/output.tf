133854c297a0bc8a3dff4d07d42104450fc8e5e6
pratheesh-new <pprasannan@cloudbourne.co>
2020-02-27 05:24:22 +0000

    minor fix

--- demo-6/module/Network/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-value = aws_vpc.test_vpc.id
-}
-output "public_subnet_id" {
-value = aws_subnet.public_subnet.id
-}
-output "sg_id" {
-value = aws_security_group.web.id
-}
--- demo-6/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_instance_ip" {
-  value = aws_instance.instance1.public_ip
-}
