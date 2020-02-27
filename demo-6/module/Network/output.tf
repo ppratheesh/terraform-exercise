3222ef4193e9725717da8ba551658837f94a2d54
pratheesh-new <pprasannan@cloudbourne.co>
2020-02-27 05:28:29 +0000

    adding demo

--- demo-6/module/Network/output.tf
+++ demo-6/module/Network/output.tf
@@ -4,9 +4,6 @@ value = aws_vpc.test_vpc.id
 output "public_subnet_id" {
 value = aws_subnet.public_subnet.id
 }
-output "private_subnet_id" {
-value = aws_subnet.private_subnet.id
-}
 output "sg_id" {
 value = aws_security_group.web.id
 }
---
+++ demo-6/module/Network/resource.tf
@@ -0,0 +1,65 @@
+resource "aws_vpc" "test_vpc" {
+  cidr_block = var.cidr_vpc
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+  Environment = var.environment_tag
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.test_vpc.id
+  tags = {
+  Environment = var.environment_tag
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id=aws_vpc.test_vpc.id
+  cidr_block=var.cidr_public_subnet
+  availability_zone=var.availability_zone_pubic
+  map_public_ip_on_launch = true
+  tags = {
+  Environment = var.environment_tag
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = var.cidr_private_subnet
+  availability_zone = var.availability_zone_private
+  tags = {
+  Environment=var.environment_tag
+  }
+}
+resource "aws_route_table" "rtb_public" {
+   vpc_id=aws_vpc.test_vpc.id
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "rta_public_association" {
+     subnet_id = aws_subnet.public_subnet.id
+     route_table_id = aws_route_table.rtb_public.id
+}
+resource  "aws_security_group" "web" {
+name = "web-SG"
+vpc_id = aws_vpc.test_vpc.id
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
